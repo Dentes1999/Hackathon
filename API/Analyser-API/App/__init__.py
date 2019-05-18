da70f53a48feddbce489a2970b2b7e07899a2479
Dentes1999 <32334702+Dentes1999@users.noreply.github.com>
2019-05-18 13:18:31 +0300

    Add files via upload

---
+++ API/Analyser-API/App/__init__.py
@@ -0,0 +1,51 @@
+from flask import Flask
+from flask_restful import Api
+from flask_whooshee import Whooshee
+from flask_compress import Compress
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_optimize import FlaskOptimize
+from flask_login import LoginManager
+from config import Config
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+api = Api(app)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+flask_optimize = FlaskOptimize()
+Compress(app)
+app.config.from_object(Config)
+
+
+
+from app import routes, models
+from app.resources import user, research
+
+
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    jti = decrypted_token['jti']
+    return models.RevokedTokenModel.is_jti_blacklisted(jti)
+
+
+api.add_resource(user.UserRegistration, '/registration')
+api.add_resource(user.UserLogin, '/login')
+api.add_resource(user.OAuthAuthorize, '/oauth/login')
+api.add_resource(user.OAuthFacebookCallback, '/oauth/facebook/callback')
+api.add_resource(user.OAuthGoogleCallback, '/oauth/google/callback')
+api.add_resource(user.UserChange, '/update_or_delete_user')
+api.add_resource(user.Followers, '/followers')
+api.add_resource(user.UserLogoutAccess, '/logout/access')
+api.add_resource(user.UserLogoutRefresh, '/logout/refresh')
+api.add_resource(user.TokenRefresh, '/token/refresh')
+api.add_resource(user.AllUsers, '/users')
+api.add_resource(user.SecretResource, '/search/users')
+api.add_resource(research.MyResearch, '/research/use')
+api.add_resource(research.ResearchSubscription, '/research/subscribe')
+api.add_resource(research.ResearchLike, '/research/like')
+api.add_resource(research.SearchResearches, '/research/search')
+api.add_resource(research.ResearchViews, '/research/views')
+api.add_resource(research.MyResearches, '/research/my')
+api.add_resource(research.UsersResearches, '/user/researches')
---
+++ API/Analyser-API/App/resources/request_parser.py
@@ -0,0 +1,48 @@
+from flask_restful import reqparse
+
+
+reg_parser = reqparse.RequestParser()
+reg_parser.add_argument('username', type=str, help = 'This field cannot be blank', required = True)
+reg_parser.add_argument('password', type=str, help = 'This field cannot be blank', required = True)
+reg_parser.add_argument('email', type=str, help = 'This field cannot be blank', required = True)
+reg_parser.add_argument('isCompany', type=bool, help = 'This field cannot be blank', required = True)
+reg_parser.add_argument('fullname', type=str, help = 'This field cannot be blank', required = True)
+
+
+login_parser = reqparse.RequestParser()
+login_parser.add_argument('user', type=str, help = 'This field cannot be blank', required = True)
+login_parser.add_argument('user_password', type=str, help = 'This field cannot be blank', required = True)
+
+
+update_user_parser = reqparse.RequestParser()
+login_parser.add_argument('username', type=str)
+login_parser.add_argument('fullname', type=str)
+login_parser.add_argument('bio', type=str)
+
+
+followers_parser = reqparse.RequestParser()
+followers_parser.add_argument('worker_id', type=int, help='This field cannot be blank', required=True)
+
+
+subscriptions_likes_parser = reqparse.RequestParser()
+subscriptions_likes_parser.add_argument('research_id', type=int, help='This field cannot be blank', required=True)
+
+
+research_filters = reqparse.RequestParser()
+research_filters.add_argument('sort_way', type=str)
+research_filters.add_argument('start_date', type=str)
+research_filters.add_argument('end_date', type=str)
+research_filters.add_argument('modules', type=str, action='append')
+
+
+create_research = reqparse.RequestParser()
+create_research.add_argument('topic', type=str, required=True, help='This field cannot be blank')
+create_research.add_argument('description', type=str)
+create_research.add_argument('keywords', type=str, action='append', required=True, help='This field cannot be blank')
+create_research.add_argument('modules', type=str, action='append', required=True, help='This field cannot be blank')
+create_research.add_argument('update_interval', type=str)
+create_research.add_argument('app_id', type=str)
+create_research.add_argument('app_name', type=str)
+create_research.add_argument('app_dev', type=str)
+create_research.add_argument('isPublic', type=bool, required=True, help='This field cannot be blank')
+create_research.add_argument('analysers', type=str, required=True, help='This field cannot be blank')
